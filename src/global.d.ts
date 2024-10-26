c6d1001ec2d9a5ca876b8040bc2426d197ea6fc2
kimzini <rlawlsdl11111@gmail.com>
2024-10-26 16:59:47 +0900

    feat: jpeg 파일 적용

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,9 @@
 declare module '*.png' {
-    const value: string;
-    export default value;
-  }
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
