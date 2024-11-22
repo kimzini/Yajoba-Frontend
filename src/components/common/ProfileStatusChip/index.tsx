271dab9e78a88eb0fe9aa08be1d27e5268809d05
kimzini <rlawlsdl11111@gmail.com>
2024-11-22 23:27:24 +0900

    style: 스타일 수정

--- src/components/common/ProfileStatusChip/index.tsx
+++ src/components/common/ProfileStatusChip/index.tsx
@@ -5,11 +5,11 @@ interface ChipProps {
   
   export const ProfileStatusChip = ({ title, number }: ChipProps) => {
     return (
-      <div className="flex max-h-[40px] items-center justify-center rounded-xs bg-neutral-90 px-[17px]">
-        <span className="text-xxsmall14 mr-3 font-medium text-neutral-30">
+      <div className="flex max-h-[40px] items-center justify-center rounded-[10px] bg-neutral-90 px-[17px]">
+        <span className="text-xxsmall14 mr-2 font-medium text-neutral-30">
           {title}
         </span>
-        <span className="text-xsmall16 font-semibold text-neutral-0">
+        <span className="text-xsmall16 font-medium text-neutral-0">
           {number}
         </span>
       </div>
